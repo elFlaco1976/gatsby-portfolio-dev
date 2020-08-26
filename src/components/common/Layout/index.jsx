356596d40e73211acf26599ea62b73f85f92d5d8
fcollazos-outlier <francisco.collazos@engineering.outlier.org>
2020-08-26 13:44:14 -0400

    Background image covers also header

--- src/components/common/Layout/index.jsx
+++ src/components/common/Layout/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Footer, Header } from 'components/theme';
-import { Intro } from 'components/landing';
+import { IntroAndHeader } from 'components/landing';
 import { Global, WrapperBody } from './styles';
 import './fonts.css';
 
 export const Layout = ({ children }) => (
   <>
     <Global />
-    <Header />
-    <Intro />
+    <IntroAndHeader />
     <WrapperBody>{children}</WrapperBody>
     <Footer />
   </>
--- src/components/landing/IntroAndHeader/styles.js
+++ src/components/landing/IntroAndHeader/styles.js
@@ -1,24 +1,9 @@
 import styled from 'styled-components';
-// import myImage from 'assets/images/camila-waz-2l5U8g4f8hQ-unsplash.jpg';
-import myImage from 'assets/illustrations/footer.svg';
 
-export const TestDiv = styled.div``;
-
-export const Test = styled.div`
-  background-image: url(${myImage});
+export const ContainerIntroAndHeader = styled.div`
+  background-image: url(${props => props.src});
   background-size: cover;
   background-position: top;
-  background-repeat: no-repeat;
-`;
-
-// it works
-export const TestImage = styled.img`
-  max-width: 100%;
-`;
-
-// it works
-export const TestImage2 = styled.div`
-  background-image: url(${props => props.src});
 `;
 
 export const IntroWrapper = styled.div`
@@ -26,12 +11,9 @@ export const IntroWrapper = styled.div`
   padding-bottom: 7.5rem;
   display: flex;
 
-  background-image: url(${props => props.src});
-  background-size: cover;
-  background-position: top;
   align-items: center;
   justify-content: space-between;
-  background-color: #2098d1;
+
   color: #ffffff;
   font-family: Montserrat;
 
